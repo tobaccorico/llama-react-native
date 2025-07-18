00494b8b8ef3ead03f287b4ebf67e52fb1aadf89
ricardo <tobaccorico@gmail.com>
2025-07-18 15:24:33 +0300

    fix

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-/**
- * @format
- */
-import {Buffer} from 'buffer';
+
 import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
+import {Buffer} from 'buffer';
+import 'react-native-reanimated';
 
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-// Mock event listener functions to prevent them from fataling.
+global.Buffer = Buffer;
+
+// Mock event listener functions to prevent them from fataling
 window.addEventListener = () => {};
 window.removeEventListener = () => {};
-window.Buffer = Buffer;
+
+if (global.ErrorUtils) {
+  const originalHandler = global.ErrorUtils.getGlobalHandler();
+  global.ErrorUtils.setGlobalHandler((error, isFatal) => {
+    // Custom error handling if needed
+    if (originalHandler) {
+      originalHandler(error, isFatal);
+    }
+  });
+}
 
 AppRegistry.registerComponent(appName, () => App);
